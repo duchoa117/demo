8481a0d95f113ed23e572a24f321b9e6b881d9d1
duchoa117 <duchoapc99@gmail.com>
2018-04-20 05:35:09 +0700

    3

--- web_demo.py
+++ web_demo.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home_page.html')
 
 if __name__ == '__main__':
-  app.run(host:'127.0.0.1', port=8000, debug=True)
+  app.run( debug=True)

ce7159621ff768d65d9e981347fc685f2d9002f9
duchoa117 <duchoapc99@gmail.com>
2018-04-23 18:20:39 +0700

    test part3

--- app.py
+++ app.py
@@ -20,70 +20,64 @@ app.config.update(dict(
 ))
 
 mail = Mail(app)
-# UPLOAD_FOLDER = "static/media/"
+
 
 ALLOWED_EXTENSIONS = set(['txt', 'jpg','png', 'jpeg', 'gif', 'pdf' ])
-# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 def allowed_filed(filename):
     check_1 = "." in filename
     check_2 = filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
     return check_1 and check_2
-# finish mission:
+# finish mission:*************
 @app.route("/")
 def index():
     msg = Message("mission", sender="duchoapc99techkids@gmail.com", recipients=["duchoapc99@gmail.com","quy.dc98@gmail.com"])
     mail.send(msg)
 
     return "aasd"
-@app.route('/finish', methods = ["GET", "POST"])
-def finish():
-    if request.method == "GET":
-        return render_template('finish.html')
-    elif request.method == "POST":
-        form = request.form
-        caption = form["caption"]
-        # file = request.files["picture"]
-        image = request.files['image']
-        image_name = image.filename
-        image_bytes = base64.b64encode(image.read())
-        image_string = image_bytes.decode()
-        session["image"] = image_bytes
-        if image  and allowed_filed(image_name):
-            # file_name = "hoa"+"1."+ str(file_name.rsplit(".", 1)[1].lower())
-            # file.save(os.path.join(app.config["UPLOAD_FOLDER"], file_name))
-            mission_updated = UserMission.objects(user = session["user_id"], completed = False).first()
-            mission_updated.update(set__caption = caption, set__image = image_string, completed = True)
-            mission_updated.save()
-
-            # picture_link = "../static/media/" + file_name
-        mission = """
-            <h3><strong>N&agrave;y người anh h&ugrave;ng <img src="https://html-online.com/editor/tinymce4_6_5/plugins/emoticons/img/smiley-kiss.gif" alt="kiss" />, nhiệm vụ tiếp theo của bạn l&agrave; :</strong></h3>
-<h3 style="text-align: center;"><strong>"{{..}}"</strong></h3>
-<p><span style="color: #0000ff;"><strong>Ch&uacute;c bạn th&agrave;nh c&ocirc;ng =))</strong></span></p>
-                """
-        app.config.update(dict(
-        DEBUG = True,
-        MAIL_SERVER = 'smtp.gmail.com',
-        MAIL_PORT = 587,
-        MAIL_USE_TLS = True,
-        MAIL_USE_SSL = False,
-        MAIL_USERNAME = 'duchoapc99techkids@gmail.com',
-        MAIL_PASSWORD = 'duchoa119',
-                            ))
-        msg = Message(mission, sender="duchoapc99techkids@gmail.com", recipients=["duchoapc99@gmail.com","quy.dc98@gmail.com"])
-        mail.send(msg)
-        return redirect(url_for("share", username = UserMission(user = sessiom["user_id"]).user.username, day = 1, caption = caption, image_bytes = image_bytes))
-        # return render_template("share.html",link_share = )
-    # return redirect("share",picture_link = picture_link)
-# @app.route("/share/<caption>/<picture_link>")
-# def share(caption,picture_link):
-# @app.route('/Congratulations/<user>/<day>/<caption>/<picture>')
-# @app.route("/share/<username>/<day>/<caption>/<picture>")
-@app.route("/share/<username>/<day>/<caption>")
-def share(username,day,caption):
-    image = session['image']
-    print(picture)
-    return render_template("share.html",username = username, day = day, caption = caption, image = image)
+# @app.route('/finish', methods = ["GET", "POST"])
+# def finish():
+#     if request.method == "GET":
+#         return render_template('finish.html')
+#     elif request.method == "POST":
+#         form = request.form
+#         caption = form["caption"]
+        # file = request.files["picture"]***************
+        # image = request.files['image']
+        # image_name = image.filename
+        # image_bytes = base64.b64encode(image.read())
+        # image_string = image_bytes.decode()
+        # session["image"] = image_bytes
+        # if image  and allowed_filed(image_name):
+            # file_name = "hoa"+"1."+ str(file_name.rsplit(".", 1)[1].lower())********
+            # file.save(os.path.join(app.config["UPLOAD_FOLDER"], file_name))*********
+#             mission_updated = UserMission.objects(user = session["user_id"], completed = False).first()
+#             mission_updated.update(set__caption = caption, set__image = image_string, completed = True)
+#             mission_updated.save()
+#
+#         mission = """
+#             <h3><strong>N&agrave;y người anh h&ugrave;ng <img src="https://html-online.com/editor/tinymce4_6_5/plugins/emoticons/img/smiley-kiss.gif" alt="kiss" />, nhiệm vụ tiếp theo của bạn l&agrave; :</strong></h3>
+# <h3 style="text-align: center;"><strong>"{{..}}"</strong></h3>
+# <p><span style="color: #0000ff;"><strong>Ch&uacute;c bạn th&agrave;nh c&ocirc;ng =))</strong></span></p>
+#                 """
+#         app.config.update(dict(
+#         DEBUG = True,
+#         MAIL_SERVER = 'smtp.gmail.com',
+#         MAIL_PORT = 587,
+#         MAIL_USE_TLS = True,
+#         MAIL_USE_SSL = False,
+#         MAIL_USERNAME = 'duchoapc99techkids@gmail.com',
+#         MAIL_PASSWORD = 'duchoa119',
+#                             ))
+#         msg = Message(mission, sender="duchoapc99techkids@gmail.com", recipients=["duchoapc99@gmail.com","quy.dc98@gmail.com"])
+#         mail.send(msg)
+#         return redirect(url_for("share", username = UserMission(user = sessiom["user_id"]).user.username, day = 1, caption = caption, image_bytes = image_bytes))
+#
+# @app.route("/share/<username>/<day>/<caption>")
+# def share(username,day,caption):
+#     image = session['image']
+#     print(picture)
+#     return render_template("share.html",username = username, day = day, caption = caption, image = image)
 
 
 if __name__ == '__main__':
